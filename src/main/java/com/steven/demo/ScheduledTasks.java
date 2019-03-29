90aa957a4fa68aef51ad5ab17de5d0efad1d88d2
steven <isguoyq@gmail.com>
2019-03-29 17:38:53 +0800

    [UPD] add scheduledTasks

---
+++ src/main/java/com/steven/demo/ScheduledTasks.java
@@ -0,0 +1,28 @@
+package com.steven.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Steven Guo
+ * @version 1.0
+ * @className ScheduledTasks
+ * @description [计划任务]
+ * @date 2019/03/29
+ */
+@Component
+public class ScheduledTasks {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
