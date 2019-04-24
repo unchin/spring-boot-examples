2bf86db698f4d8f280feddd4bf148d82d7278e06
StevenGuo <isguoyq@gmail.com>
2019-04-24 10:12:32 +0800

    [UPD] Consuming a RESTful Web Service 0.1

---
+++ src/main/java/com/steven/demo/Application.java
@@ -0,0 +1,25 @@
+package com.steven.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+    }
+
+}
