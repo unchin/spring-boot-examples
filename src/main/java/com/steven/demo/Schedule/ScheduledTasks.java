f6950f77be2f0a5a1f37106c088a507311eff764
StevenGuo <isguoyq@gmail.com>
2019-06-14 16:37:50 +0800

    [UPD]add shiro

--- src/main/java/com/steven/demo/Schedule/ScheduledTasks.java
+++ src/main/java/com/steven/demo/Schedule/ScheduledTasks.java
@@ -15,13 +15,13 @@ import java.util.Date;
  * @description [计划任务]
  * @date 2019/03/29
  */
-@Component
+//@Component
 public class ScheduledTasks {
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
---
+++ src/main/java/com/steven/demo/shiro/MyRealm.java
@@ -0,0 +1,25 @@
+package com.steven.demo.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class MyRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = authenticationToken.getPrincipal().toString();
+        if (!"steven".equals(username)) {
+            throw new AuthenticationException("acount is not exist");
+        }
+        return new SimpleAuthenticationInfo(username, "123", getName());
+    }
+}
