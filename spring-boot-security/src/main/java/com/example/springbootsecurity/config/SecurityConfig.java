54082eae30b9adfb3976075ff621c18c7c1b6626
unchin <isguoyq@gmail.com>
2020-04-03 15:25:26 +0800

    [UPD] add todo

--- spring-boot-security/src/main/java/com/example/springbootsecurity/config/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/example/springbootsecurity/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
