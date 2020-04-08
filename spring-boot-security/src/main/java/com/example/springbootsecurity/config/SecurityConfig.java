1c9904daf7e0437a849225fd1d2f147d502cf1ee
unchin <isguoyq@gmail.com>
2020-04-08 16:33:49 +0800

    [UPD] update docker

--- spring-boot-security/src/main/java/com/example/springbootsecurity/config/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/example/springbootsecurity/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import java.io.PrintWriter;
-import java.time.LocalDate;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
