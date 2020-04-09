650c14da5560145d1c642c8f6ed3e886f49d03a0
unchin <isguoyq@gmail.com>
2020-04-09 10:29:25 +0800

    [UPD] add spring-boot-docker-multi-environment 0.1

---
+++ spring-boot-docker-multi-environment/src/main/java/unchin/springbootdockermultienvironment/SpringBootDockerMultiEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package unchin.springbootdockermultienvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMultiEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerMultiEnvironmentApplication.class, args);
+    }
+
+}
---
+++ spring-boot-docker-multi-environment/src/test/java/unchin/springbootdockermultienvironment/SpringBootDockerMultiEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package unchin.springbootdockermultienvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMultiEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
