7eafe30d0172f89b21942c02af091b064e03746b
unchin <isguoyq@gmail.com>
2020-04-14 10:36:08 +0800

    [UPD] add spring-boot-file

---
+++ spring-boot-file/src/main/java/unchin/springbootfile/SpringBootFileApplication.java
@@ -0,0 +1,13 @@
+package unchin.springbootfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFileApplication.class, args);
+    }
+
+}
---
+++ spring-boot-file/src/test/java/unchin/springbootfile/SpringBootFileApplicationTests.java
@@ -0,0 +1,13 @@
+package unchin.springbootfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
